7e9a24767f7956e7727b0177ad5dc0ec5a9851a5
potionPI <potion080pi@gmail.com>
2017-04-02 13:20:31 -0400

    Create page9cycle.js

---
+++ scripts/page9cycle.js
@@ -0,0 +1,8 @@
+window.onkeyup = function(e) {
+	var event = e.which || e.keyCode || 0;
+
+	if (event == 27) { // ESC Key
+		window.location.href = 'https://potionpi.github.io/thegreatgatsby/hiddensun.html'; // Navigate to URL
+	}
+
+}
